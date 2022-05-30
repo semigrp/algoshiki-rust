cdaf8db7a22f7ce9cf497af997852ccc9e41bdd7
semigrp <semigroupoid@gmail.com>
2022-05-30 19:20:27 +0900

    2-2

--- stdio/2-1.rs
+++ stdio/2-1.rs
@@ -1,13 +1,19 @@
 use std::io;
 
 fn main() {
-    let s = get_input().split(' ');
-    let v: Vec<&'static str> = s.split_whitespaces.collect();
-    println!("{}", s);
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
+
+    let inputs: Vec<u8> = line
+        .split_whitespace()
+        .map(|x| x.parse().expect("Not an integer!"))
+        .collect();
+
+    let res = inputs[0] + inputs[1];
+
+    println!("{}", res);
 }
 
-fn get_input() -> String {
-    let mut number = String::new();
-    io::stdin().read_line(&mut number).ok();
-    return number.trim().parse().ok().unwrap();
-}
+// inputs is a Vec<u32> of the inputs.
